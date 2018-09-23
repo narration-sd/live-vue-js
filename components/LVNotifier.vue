b611341cda54f77825f08ddedc2450eda59796af
Narration SD <narrationsd@gmail.com>
2018-09-23 01:30:30 -0700

    Add LVNotifier component, taking  care of all including Vue plugin. A next step could be own formatted dialog, but this is fine.

---
+++ components/LVNotifier.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-dialog/>
+</template>
+
+<script>
+import Vue from 'vue'
+import { ReportModal } from '@/live-vue/plugins/report-modal'
+import VModal from 'vue-js-modal'
+
+Vue.use(ReportModal)
+Vue.use(VModal, { dialog: true })
+
+export default {
+  name: 'LVNotifier'
+}
+</script>
+
+<style>
+
+  .v--modal {
+     background: #0c5460 !important;
+     color: lightgoldenrodyellow !important;
+  }
+
+</style>
