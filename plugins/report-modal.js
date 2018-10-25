17f1868428d64b88e47c04045406276152337acc
Narration SD <narrationsd@gmail.com>
2018-10-24 21:51:52 -0700

    small things on report-modal.js

--- plugins/report-modal.js
+++ plugins/report-modal.js
@@ -1,7 +1,5 @@
-import helpers from '@/live-vue-js/helpers.js'
 export const ReportModal = {
-  // requires npm install vue-js-modal
-  // uses liveVue prefix to stay out of other packages
+  // requires npm install vue-js-mod
   install (Vue, options) {
     Vue.mixin({
       data () {
