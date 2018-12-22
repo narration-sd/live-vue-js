96af55765c227e8296a2bf280ff313e1fe5e90ac
Narration SD <narrationsd@gmail.com>
2018-12-22 10:59:04 -0800

    add react Reporter, first workable stage

---
+++ react/Reporter.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import LvModal from './lv-modal.jsx'
+
+class Reporter extends Component {
+  constructor (props) {
+    super(props)
+    this.reporter = React.createRef()
+  }
+
+  state = { show: false, content: 'This is parent state content' }
+
+  report = (title, content) => {
+    this.reporter.current.report(title, content)
+  }
+
+  render () {
+    return (
+      <LvModal ref={this.reporter}
+               subtitle={"some subtitle or another"}></LvModal>
+    )
+  }
+}
+
+export default Reporter
---
+++ react/lv-modal.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Modal from 'react-modal'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '80%',
+    background: '#0c5460',
+    color: 'lightgoldenrodyellow'
+  },
+  html: {
+    // fontsize: '14px',
+    font: '70% sans-serif'
+  }
+}
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('body') // this is actually the default if don't set
+
+class LvModal extends React.Component {
+
+  constructor ({ subtitle }) {
+    super()
+
+    this.state = {
+      modalIsOpen: false,
+      content: 'no content yet'
+    }
+
+    this.openModal = this.openModal.bind(this)
+    this.afterOpenModal = this.afterOpenModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  report = (title, content) => {
+    this.setState({ modalIsOpen: true, title: title, content: content })
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true, title: '', content: '' })
+  }
+
+  afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00'
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false })
+  }
+
+  render () {
+
+    return (
+      <div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello from
+            subtitle</h2>
+          <h2>{this.state.title} </h2>
+          <p>{this.state.content}</p>
+          <div
+            dangerouslySetInnerHTML={{ __html: 'inner: ' + this.state.content }}></div>
+          <br/>
+          <button onClick={this.closeModal}>Accept</button>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default LvModal
+
