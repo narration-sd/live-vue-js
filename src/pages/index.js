9c6576f2152fda163079e5a751f4a12186f093c9
Narration SD <narrationsd@gmail.com>
2019-02-06 00:14:48 -0800

    added cheese-plates index, today's lv-gatsby-index at point via comments, npm and composer updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
+
   <Layout>
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
