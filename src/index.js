fa54114bb09d1972af70ad22c9edbcdbc67f7c8a
Narration SD <narrationsd@gmail.com>
2019-05-26 13:01:35 -0700

    update copyrights, package locations, version

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+/*
+ *  @link      https://narrationsd.com/
+ *  @copyright Copyright (c) 2018 Narration SD
+ *  @license   Craft License https://narrationsd.com/docs/license.html
+ */
+
 // future component groups add this same way
 export * from './react/LiveVueGatsby.jsx'
